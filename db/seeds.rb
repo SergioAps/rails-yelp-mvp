43cc8e51775d2f2a44d006a7237581b1845c89e0
Eduardo Aguilar <laloaps@gmail.com>
2022-08-17 13:22:38 -0400

    index restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning DB"
+Restaurant.destroy_all
+puts "Creating 5 Restaurants"
+Restaurant.create!(name:"La Cazona", address:"Xalapa", phone_number:"283-1008299", category:"italian")
+Restaurant.create!(name:"Casa Blanca", address:"Xalapa", phone_number:"283-1063567", category:"french")
+Restaurant.create!(name:"Wok", address:"Veracruz", phone_number:"283-1112290", category:"chinese")
+Restaurant.create!(name:"Asadero Cien", address:"Orizaba", phone_number:"555-1008210", category:"french")
+Restaurant.create!(name:"Ratatoui", address:"Cordoba", phone_number:"283-2008288", category:"french")
+puts "Creado"
