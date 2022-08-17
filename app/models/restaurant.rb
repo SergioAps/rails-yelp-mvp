915357d2e675501124841816949617b472d53f8e
Eduardo Aguilar <laloaps@gmail.com>
2022-08-17 12:32:49 -0400

    validates Models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
-  CATEGORY = %(chinese italian japanese french belgian)
+  # CATEGORY = %I(chinese italian japanese french belgian)
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: %w[chinese italian japanese french belgian] }
 end
---
+++ db/migrate/20220817155309_change_phone_number_to_text.rb
@@ -0,0 +1,5 @@
+class ChangePhoneNumberToText < ActiveRecord::Migration[7.0]
+  def change
+    change_column :restaurants, :phone_number, :text
+  end
+end
